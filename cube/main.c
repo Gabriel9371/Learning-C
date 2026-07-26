6347b3ccd49782bf904f83923ce12af42fdb26f7
D. Gabrielz <mendesdouglasgabriel30@gmail.com>
2026-07-26 18:01:27 -0300

    f: Adicionada uma função de projeção

--- cube/main.c
+++ cube/main.c
@@ -5,18 +5,57 @@
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
 
+
+#define WIDTH 800
+#define HEIGHT 600
+
+
+typedef struct{
+  float x, y, z;
+}Point3D;
+
+
+typedef struct{
+  int x, y;
+}Point2D;
+
+Point2D cordenadas(Point3D p){
+  int x = (p.x * 200) + 400;
+  int y = ((p.y * 200) * -1) + 300;
+
+  Point2D p22;
+  p22.x = x;
+  p22.y = y;
+
+  return p22;
+}
+
 int main(){
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window* window = SDL_CreateWindow(
     "Janela",
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
-    800, 600,
+    WIDTH, HEIGHT,
     SDL_WINDOW_SHOWN
   );
   
   SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   SDL_Event e;
+
+  float z1 = 1.0f;
+  float z2 = -1.0f;
+  Point3D cube[8] = {
+    {1, 1, z1},
+    {1, -1, z1},
+    {-1, 1, z1},
+    {-1, -1, z1},
+    {1, 1, z2},
+    {1, -1, z2},
+    {-1, 1, z2},
+    {-1, -1, z2}
+
+  };
 
   bool runing = false;
   while (!runing) {
